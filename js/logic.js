721f2b77137f607364c40416100fa37aef61cf7c
Alexander <kasyanov.a@internet.ru>
2023-01-25 14:09:25 +0700

    calc

---
+++ js/logic.js
@@ -0,0 +1,83 @@
+import { print } from './utils/print.js'
+
+const main = () => {
+
+    let result = 0;
+    let firstOperand = 0;
+    let operation;
+
+    return (state) => {
+        
+        result = result + state;
+
+        if (result.charAt(0) == '0') 
+        result = result.slice(1);
+
+        if (state === '.' && result.includes('.')) return
+
+        if(result.includes('АС')) {
+            result = 0; 
+        } else if (result.includes('С')) {
+            result = result.slice(0, -2);
+        }
+
+        print(result)
+
+        if (state == '+' || state == '-' || state == '/' || state == 'x') { 
+            operation = state;
+            firstOperand = result;
+            result = 0;
+            print(state)
+        } else if (state == '=') {
+            if (operation == '+') {
+                result = parseFloat(firstOperand) + parseFloat(result);
+                print(result)
+            } else if (operation == '-') {
+                result = parseFloat(firstOperand) - parseFloat(result);
+                print(result)
+            } else if (operation == '/') {
+                result = parseFloat(firstOperand) / parseFloat(result);
+                print(result)
+            } else if (operation == 'x') {
+                result = parseFloat(firstOperand) * parseFloat(result);
+                print(result)
+            }
+        }
+        
+    }
+
+}
+
+export default main
+
+
+
+        
+            // switch(operation) {
+            //     case '+':
+            //         proccess = prevOperand + currentOperand
+            //         break
+            //     case '-':
+            //         proccess = prevOperand - currentOperand
+            //         break
+            //     case 'x':
+            //         proccess = prevOperand * currentOperand
+            //         break
+            //     case '/':
+            //         proccess = prevOperand / currentOperand
+            //         break
+            
+            //     default: return
+            // }
+        
+            // result = proccess;
+
+            
+
+            // currentOperand = 0;
+            // prevOperand = 0;
+        
+            // console.log("operation:" + operation)
+            // console.log("state:" + state)
+            // console.log("result:" + result)
+            // console.log("prevOperand:" + prevOperand)
